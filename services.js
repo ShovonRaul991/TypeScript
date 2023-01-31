405879ada65f2fa7de585737e716699a09de77d2
Shovon Raul <shovonraul18@gmail.com>
2023-01-31 13:20:32 +0530

    responsive

--- services.js
+++ services.js
@@ -1,34 +1,35 @@
-var allCheckbox = document.getElementById("form-checkbox");
-var checkbox = document.getElementById("checkbox");
-var identity = /** @class */ (function () {
-    function identity(name, score, email) {
+var person = /** @class */ (function () {
+    function person(name, score, email) {
         this.name = name;
-        this.score = score;
         this.email = email;
+        this.score = score;
     }
-    ;
-    identity.prototype.getName = function () {
-        return this.name;
-    };
-    identity.prototype.getEmail = function () {
-        return this.email;
-    };
-    identity.prototype.getScore = function () {
-        return this.score;
-    };
-    identity.prototype.setName = function (name) {
-        this.name = name;
-    };
-    identity.prototype.setEmail = function (email) {
-        this.email = email;
-    };
-    identity.prototype.setScore = function (score) {
-        this.score = score;
-    };
-    return identity;
+    return person;
 }());
-function checkboxChecking() {
-    if (allCheckbox) {
-        checkbox.checked;
+var person1 = new person('Vijay Prakash', 34, 'vijay@technovert.com');
+var person2 = new person('Sashi Pagadala', 21, 'sashi@technovert.com');
+var person3 = new person(null, null, null);
+var person4 = new person(null, null, null);
+var person5 = new person(null, null, null);
+var person6 = new person(null, null, null);
+var person7 = new person(null, null, null);
+var person8 = new person(null, null, null);
+var IdentityArray = [person1, person2, person3, person4, person5, person6, person7, person8];
+function addRow() {
+    var myTable = document.getElementById("service-table");
+    for (var i = 0; i < IdentityArray.length; i++) {
+        var row = myTable.insertRow(-1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        var cell5 = row.insertCell(4);
+        //checkbox creation
+        var cBox = document.createElement('input');
+        cBox.setAttribute('type', 'checkbox');
+        cell1.appendChild(cBox);
+        cell2.innerHTML = IdentityArray[i].name;
+        cell3.innerHTML = String(IdentityArray[i].score);
+        cell4.innerHTML = IdentityArray[i].email;
     }
 }
--- services.ts
+++ services.ts
@@ -1,40 +1,43 @@
-let allCheckbox = document.getElementById("form-checkbox") as HTMLInputElement | null;
-let checkbox = document.getElementById("checkbox") as HTMLInputElement | null;
-
-class identity{
-    public constructor(protected name:String,protected score:number,protected email:string){};
-
-    public getName(){
-        return this.name;
-    }
-
-    public getEmail(){
-        return this.email;
-    }
-
-    public getScore(){
-        return this.score;
-    }
-
-    public setName(name: string){
+class person{
+    name: string ; 
+    email: string ;
+    score: number;
+    constructor(name?:string,score?:number,email?:string){
         this.name = name;
-    }
-
-    public setEmail(email: string){
         this.email = email;
-    }
-
-    public setScore(score: number){
         this.score = score;
     }
 }
 
-class person extends identity{
-    
-}
+let person1 = new person('Vijay Prakash',34,'vijay@technovert.com');
+let person2 = new person('Sashi Pagadala',21,'sashi@technovert.com');
+let person3 = new person(null,null,null);
+let person4 = new person(null,null,null);
+let person5 = new person(null,null,null);
+let person6 = new person(null,null,null);
+let person7 = new person(null,null,null);
+let person8 = new person(null,null,null);
 
-function checkboxChecking(){
-    if(allCheckbox){
-        checkbox.checked;
-    }
+let IdentityArray = [person1,person2,person3,person4,person5,person6,person7,person8];
+
+function addRow(){
+    let myTable = document.getElementById("service-table") as HTMLTableElement;
+     for(let i =0;i<IdentityArray.length;i++){
+        let row = myTable.insertRow(-1);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+        let cell4 = row.insertCell(3);
+        let cell5 = row.insertCell(4);
+        
+        //checkbox creation
+        var cBox = document.createElement('input');
+        cBox.setAttribute('type', 'checkbox');
+        cell1.appendChild(cBox);
+
+        cell2.innerHTML = IdentityArray[i].name;
+        cell3.innerHTML = String(IdentityArray[i].score);
+        cell4.innerHTML = IdentityArray[i].email;
+     }
+
 }
